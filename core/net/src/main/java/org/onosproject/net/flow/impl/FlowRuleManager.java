a44312783e4be38a4626a889eeb8962a351885e0
Matt Jeanneret <mjeanner@gmail.com>
2016-02-10 10:56:19 -0500

    Revert "added logging to FlowEntry exactMatch"
    This reverts commit 803b1b434fc6d49eb419ba76002a4fd044127131.

--- core/net/src/main/java/org/onosproject/net/flow/impl/FlowRuleManager.java
+++ core/net/src/main/java/org/onosproject/net/flow/impl/FlowRuleManager.java
@@ -64,7 +64,11 @@ import org.onosproject.net.provider.AbstractProviderService;
 import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
