9d998a18a8178dbfe8c44ea75f440be88bb09129
nick <np202g@att.com>
2016-06-14 16:34:21 -0400

    Multiplication of traffic app

---
+++ apps/testing-app/src/main/java/ofdpagroups/L2InterfaceGroup.java
@@ -0,0 +1,63 @@
+package ofdpagroups;
+
+import org.onosproject.core.ApplicationId;
+import org.onosproject.core.GroupId;
+import org.onosproject.net.DeviceId;
+import org.onosproject.net.PortNumber;
+import org.onosproject.net.flow.DefaultTrafficTreatment;
+import org.onosproject.net.flow.TrafficTreatment;
+import org.onosproject.net.group.*;
+
+import java.nio.ByteBuffer;
+import java.util.Collections;
+
+/**
+ * Created by nick on 2/9/16.
+ */
+public class L2InterfaceGroup {
+
+    private static ApplicationId appId;
+    private static GroupService groupService;
+
+    public static void initiate(ApplicationId appId, GroupService groupService){
+        L2InterfaceGroup.appId = appId;
+        L2InterfaceGroup.groupService = groupService;
+
+    }
+
+
+
+    public static GroupKey key(int portNumber, int vlanId){
+
+        Integer integerGroupId =  ((vlanId <<  16) | portNumber);
+        return new DefaultGroupKey(ByteBuffer.allocate(4).putInt(integerGroupId).array());
+    }
+
+    public static GroupId create(int portNumber, int vlanId, boolean popVlan, DeviceId device){
+
+        TrafficTreatment.Builder treatment = DefaultTrafficTreatment.builder();
+        treatment.setOutput(PortNumber.portNumber(portNumber));
+        if(popVlan){
+            treatment.popVlan();
+        }
+
+
+
+        Integer integerGroupId =  ((vlanId <<  16) | portNumber);
+        final GroupKey groupkey = new DefaultGroupKey(ByteBuffer.allocate(4).putInt(integerGroupId).array());
+
+        GroupBucket bucket =
+                DefaultGroupBucket.createIndirectGroupBucket(treatment.build());
+        GroupDescription groupDescription = new DefaultGroupDescription(device,
+                GroupDescription.Type.INDIRECT,
+                new GroupBuckets(Collections.singletonList(bucket)),
+                groupkey,
+                integerGroupId,
+                appId);
+        groupService.addGroup(groupDescription);
+
+        return groupService.getGroup(device, groupkey).id();
+
+    }
+
+}
--- apps/testing-app/src/main/java/org/onosproject/testing/TestingComponent.java
+++ apps/testing-app/src/main/java/org/onosproject/testing/TestingComponent.java
@@ -1,6 +1,7 @@
 
 package org.onosproject.testing;
 
+import ofdpagroups.GroupFinder;
 import org.apache.felix.scr.annotations.*;
 
 
@@ -10,7 +11,7 @@ import org.onosproject.core.ApplicationId;
 import org.onosproject.core.CoreService;
 import org.onosproject.driver.extensions.NoviflowPopVxLan;
 import org.onosproject.driver.extensions.NoviflowSetVxLan;
-import org.onosproject.translation.FlowRuleTranslation;
+import org.onosproject.driver.extensions.OfdpaMatchVlanVid;
 import org.onosproject.net.DeviceId;
 import org.onosproject.net.PortNumber;
 import org.onosproject.net.device.DeviceService;
@@ -54,8 +55,15 @@ public class TestingComponent {
 
 
     static ApplicationId appId;
-    static final DeviceId deviceId = DeviceId.deviceId("of:000000223d5a00d9");
+    static final DeviceId deviceId = DeviceId.deviceId("of:000000000000da7a");
     static final DeviceId ovsId = DeviceId.deviceId("of:0000b214876af640");
+
+
+    static final int ACL_TABLE = 60;
+    static final int VLAN_TABLE = 10;
+    static final int TMAC_TABLE = 20;
+    static final int UNICAST_ROUTING_TABLE = 30;
+    static final int BRIDGING_TABLE = 50;
 
 
     @Activate
@@ -65,11 +73,24 @@ public class TestingComponent {
         appId = coreService.registerApplication("org.onosproject.testing");
 
 
+        List<PortNumber> generatorPorts = new LinkedList<>();
+        generatorPorts.add(PortNumber.portNumber(33));
+        generatorPorts.add(PortNumber.portNumber(34));
+        generatorPorts.add(PortNumber.portNumber(35));
+        generatorPorts.add(PortNumber.portNumber(36));
+
+        List<PortNumber> shitHookPorts = new LinkedList<>();
+        shitHookPorts.add(PortNumber.portNumber(10));
+        shitHookPorts.add(PortNumber.portNumber(12));
+
+        multiplyTraffic(generatorPorts, shitHookPorts, PortNumber.portNumber(32), deviceId);
+
+
         //linkPorts(1, 2);
         //ipFlow(Ip4Address.valueOf("192.168.1.4"), 3);
         //vxlanTest(1, 2, VlanId.vlanId((short)5));
 
-        Ip4Address vxlanSrc = Ip4Address.valueOf("10.21.12.13");
+       /* Ip4Address vxlanSrc = Ip4Address.valueOf("10.21.12.13");
         Ip4Address vxlanDst = Ip4Address.valueOf("10.55.12.13");
         MacAddress vxlanMacDst = MacAddress.valueOf("11:11:11:11:11:11");
         MacAddress vxlanMacSrc = MacAddress.valueOf("22:22:22:22:22:22");
@@ -83,7 +104,7 @@ public class TestingComponent {
 
         inbandOltControl();
 
-        ovsTest(ovsId);
+        ovsTest(ovsId);*/
 
 
     }
@@ -129,6 +150,101 @@ public class TestingComponent {
         addFlow(selector3.build(), outPort1.build(), 500, ovsId);
 
     }
+
+    private FlowRule vlanTableFlows(PortNumber port, VlanId vlanId, DeviceId deviceId){
+
+        TrafficSelector.Builder vlanTableSelector = DefaultTrafficSelector.builder();
+        vlanTableSelector.matchInPort(port);
+        vlanTableSelector.extension(new OfdpaMatchVlanVid(vlanId), deviceId);
+
+        TrafficTreatment.Builder vlanTableTreatment = DefaultTrafficTreatment.builder();
+        vlanTableTreatment.transition(TMAC_TABLE);
+
+        FlowRule.Builder vlanTableRule = DefaultFlowRule.builder();
+        vlanTableRule.withSelector(vlanTableSelector.build());
+        vlanTableRule.withTreatment(vlanTableTreatment.build());
+        vlanTableRule.withPriority(8);
+        vlanTableRule.forTable(VLAN_TABLE);
+        vlanTableRule.fromApp(appId);
+        vlanTableRule.forDevice(deviceId);
+        vlanTableRule.makePermanent();
+
+        return vlanTableRule.build();
+
+    }
+
+    private void multiplyTraffic (List<PortNumber> inPorts, List<PortNumber> shitHookPorts, PortNumber outPort, DeviceId deviceId) {
+        // Assuming shithook port n is connected to port n+1
+
+        //Cumulus vlans : 3793
+        for(int i = 2; i< 3793; i++) {
+
+            VlanId vlan = VlanId.vlanId((short)i);
+
+            for(PortNumber port : inPorts) {
+                flowRuleService.applyFlowRules(vlanTableFlows(port, vlan, deviceId));
+            }
+            multicastTraffic(inPorts, shitHookPorts, vlan, deviceId);
+            mergeTraffic(shitHookPorts, outPort, vlan, deviceId);
+
+        }
+
+    }
+
+    private void multicastTraffic(List<PortNumber> inPorts, List<PortNumber> outPorts, VlanId vlanId, DeviceId deviceId) {
+
+        for(PortNumber in : inPorts) {
+
+            TrafficSelector.Builder selector = DefaultTrafficSelector.builder();
+            selector.matchInPort(in);
+            selector.extension(new OfdpaMatchVlanVid(vlanId), deviceId);
+
+            TrafficTreatment.Builder treatment = DefaultTrafficTreatment.builder();
+            treatment.group(GroupFinder.getL2Multicast(outPorts, vlanId, deviceId));
+
+            FlowRule.Builder rule = DefaultFlowRule.builder();
+            rule.withSelector(selector.build());
+            rule.withTreatment(treatment.build());
+            rule.withPriority(45000);
+            rule.forTable(ACL_TABLE);
+            rule.fromApp(appId);
+            rule.forDevice(deviceId);
+            rule.makePermanent();
+
+            flowRuleService.applyFlowRules(rule.build());
+
+        }
+
+
+    }
+
+    private void mergeTraffic(List<PortNumber> toAggregatePorts, PortNumber outputPort, VlanId vlanId, DeviceId deviceId) {
+
+        for(PortNumber shitHook : toAggregatePorts) {
+
+            PortNumber port = PortNumber.portNumber(shitHook.toLong() + 1);
+
+
+            TrafficSelector.Builder selector = DefaultTrafficSelector.builder();
+            selector.matchInPort(port);
+            selector.extension(new OfdpaMatchVlanVid(vlanId), deviceId);
+
+            TrafficTreatment.Builder treatment = DefaultTrafficTreatment.builder();
+            treatment.group(GroupFinder.getL2Interface(outputPort, vlanId, deviceId));
+
+            FlowRule.Builder rule = DefaultFlowRule.builder();
+            rule.withSelector(selector.build());
+            rule.withTreatment(treatment.build());
+            rule.withPriority(46000);
+            rule.forTable(ACL_TABLE);
+            rule.fromApp(appId);
+            rule.forDevice(deviceId);
+            rule.makePermanent();
+        }
+
+
+    }
+
 
     private void addFlow(TrafficSelector selector, TrafficTreatment treatment, int priority, DeviceId deviceId) {
 
