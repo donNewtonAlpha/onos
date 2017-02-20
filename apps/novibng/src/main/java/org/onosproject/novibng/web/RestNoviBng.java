c93def7727bd241b75da91bd196b27a17276c335
nick <np202g@att.com>
2017-02-20 12:11:00 -0500

    pom tinkering and checkstyle

--- apps/novibng/src/main/java/org/onosproject/novibng/web/RestNoviBng.java
+++ apps/novibng/src/main/java/org/onosproject/novibng/web/RestNoviBng.java
@@ -28,7 +28,11 @@ import org.slf4j.Logger;
 import org.onosproject.novibng.NoviBngComponent;
 import org.onosproject.novibng.config.BngDeviceConfig;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.POST;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
